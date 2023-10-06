c9bbc5aad68253c659dbfc22a37a0aa2df6978f6
Vageesha Choudhary <vageesha.choudhary@gmail.com>
2023-10-06 13:59:35 -0700

    [SMART-7] login page changes

---
+++ src/components/Navbar.vue
@@ -0,0 +1,179 @@
+<template>
+  <header>
+    <div class="logo">
+      <ul class="nav-ul">
+        <li class="nav-li">
+          <router-link class="nav--link" to="/">Home</router-link>
+        </li>
+      </ul>
+    </div>
+    <span class="menu-toggle" @click="toggleMenu">+</span>
+    <nav class="nav" v-bind:class="{ active: menuOpen }">
+      <span class="close" @click="toggleMenu">X</span>
+      <ul class="nav-ul">
+        <!-- <li class="nav-li">
+          <router-link class="nav--link" to="/login">Login</router-link>
+        </li> -->
+        <li class="nav-li">
+          <router-link class="btn btn-main" to="/login">Login</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class Navbar extends Vue {
+  menuOpen = false;
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+}
+</script>
+
+<style scoped lang="scss">
+header {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0.5rem 2rem;
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.nav-ul {
+  flex: 1;
+  list-style: none;
+  display: flex;
+}
+
+.nav-li:not(:last-child) {
+  margin-right: 1rem;
+}
+
+.nav--link {
+  display: block;
+  padding: 1rem 2rem;
+
+  text-decoration: none;
+
+  text-transform: uppercase;
+  font-weight: 700;
+  color: var(--white-color);
+  justify-content: center;
+  align-items: center;
+
+  transition: all 0.5s ease-in-out;
+  position: relative;
+}
+
+.nav--link:hover {
+  letter-spacing: 1px;
+}
+
+.nav--link::before,
+.nav--link::after {
+  visibility: hidden;
+  content: "";
+  margin: 0 auto;
+  display: block;
+  border: 1px solid var(--white-color);
+  transition: width 0.5s ease-in-out;
+  position: absolute;
+  width: 0;
+}
+
+.nav--link::before {
+  right: 0;
+  left: 0;
+  top: 0;
+}
+
+.nav--link::after {
+  right: 0;
+  left: 0;
+  bottom: 0;
+}
+
+.nav--link:hover:before,
+.nav--link:hover:after {
+  visibility: visible;
+  width: 80%;
+}
+
+.logo {
+  letter-spacing: 2px;
+  font-weight: 900;
+  font-size: 1.5rem;
+  font-family: "Helvetica", Roboto;
+  color: var(--white-color);
+  cursor: pointer;
+}
+
+.menu-toggle,
+.nav .close {
+  display: none;
+}
+
+@media (max-width: 800px) {
+  .menu-toggle {
+    color: var(--white-color);
+    background: tomato;
+    padding: 0.5rem;
+    cursor: pointer;
+    display: block;
+  }
+
+  .nav {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100vw;
+    height: 100vh;
+    background: #000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.6s cubic-bezier(0.86, 0, 0.07, 1);
+    transform: translate3d(-100%, 0, 0);
+  }
+
+  .nav.active {
+    transform: translateZ(0);
+  }
+
+  .nav .close {
+    color: var(--white-color);
+    position: absolute;
+    top: 2rem;
+    right: 2rem;
+    width: 40px;
+    height: 40px;
+    font-weight: 900;
+
+    background: tomato;
+
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
+
+  .nav-ul {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .nav--link:not(:first-child),
+  .btn {
+    margin-top: 2rem;
+  }
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <div>
+        <label for="username">Username:</label>
+        <input type="text" id="username" v-model="username" />
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class LoginView extends Vue {
+  username = "";
+  password = "";
+
+  // Methods for handling user interactions
+  login() {
+    // Handle login logic here, e.g., make" an API request to authenticate
+    console.log("Logging in with username:", this.username);
+    console.log("Password:", this.password);
+
+    // You can perform your authentication logic here and update the UI accordingly
+  }
+}
+</script>
