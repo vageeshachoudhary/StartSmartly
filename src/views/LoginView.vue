d611de89cf8a280b8ab0106d60f1d55ea40dc522
Vageesha Choudhary <vageesha.choudhary@gmail.com>
2023-10-18 10:24:54 -0700

    changes

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -17,24 +17,17 @@
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue";
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
 export default class LoginView extends Vue {
   username = "";
   password = "";
 
   // Methods for handling user interactions
   login() {
-    // Handle login logic here, e.g., make" an API request to authenticate
+
     console.log("Logging in with username:", this.username);
     console.log("Password:", this.password);
-
-    // You can perform your authentication logic here and update the UI accordingly
+    
   }
 }
 </script>
